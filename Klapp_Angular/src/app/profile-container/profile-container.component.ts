c6b7742036bb69ccc6b3b8f24b8ac792c8ed84aa
Janaína Gomes <jana.gomes90@hotmail.com>
2024-10-29 18:43:21 -0300

    up tela profile

--- Klapp_Angular/src/app/profile-container/profile-container.component.ts
+++ Klapp_Angular/src/app/profile-container/profile-container.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile-container',
@@ -7,20 +6,52 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrl: './profile-container.component.scss'
 })
 export class ProfileContainerComponent {
-  formulario: FormGroup;
-  ra: string = '123456'; // Número de RA fixo
+  nome: string = '';
+  email: string = '';
+  fixedNumber: number = 12345; // Número fixo que não pode ser alterado
 
-  constructor(private fb: FormBuilder) {
-    this.formulario = this.fb.group({
-      nome: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      ra: [{ value: this.ra, disabled: true }] // Campo RA desabilitado
-    });
-  }
+  // Notas para os três anos
+  nota1: number | null = null; // Primeiro ano
+  nota2: number | null = null;
+  nota3: number | null = null;
+  nota4: number | null = null;
+
+  nota1_2: number | null = null; // Segundo ano
+  nota2_2: number | null = null;
+  nota3_2: number | null = null;
+  nota4_2: number | null = null;
+
+  nota1_3: number | null = null; // Terceiro ano
+  nota2_3: number | null = null;
+  nota3_3: number | null = null;
+  nota4_3: number | null = null;
 
   onSubmit() {
-    if (this.formulario.valid) {
-      console.log(this.formulario.value);
-    }
+    const formData = {
+      nome: this.nome,
+      email: this.email,
+      numeroFixo: this.fixedNumber,
+      notas: {
+        primeiroAno: {
+          nota1: this.nota1,
+          nota2: this.nota2,
+          nota3: this.nota3,
+          nota4: this.nota4
+        },
+        segundoAno: {
+          nota1: this.nota1_2,
+          nota2: this.nota2_2,
+          nota3: this.nota3_2,
+          nota4: this.nota4_2
+        },
+        terceiroAno: {
+          nota1: this.nota1_3,
+          nota2: this.nota2_3,
+          nota3: this.nota3_3,
+          nota4: this.nota4_3
+        }
+      }
+    };
+    console.log(formData);
   }
 }
